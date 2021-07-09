8475deeceb84cf6ed68a111809474eb0a7be8990
alin510 <alin510@gmail.com>
2021-07-09 16:52:12 +0800

    test hello

--- app.py
+++ app.py
@@ -37,9 +37,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = "中文"
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text= r ))
 
 
 if __name__ == "__main__":
